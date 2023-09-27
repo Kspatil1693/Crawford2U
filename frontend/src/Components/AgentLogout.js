5fbb7745820f3cc681813cf9e14a50a8b2c0461d
Karishma Pawar <kspatil1693@gmail.com>
2023-09-27 14:36:38 +0530

    one

--- frontend/src/Components/AgentLogout.js
+++ frontend/src/Components/AgentLogout.js
@@ -15,7 +15,7 @@ const AgentLogout = () => {
     const handleSubmit = async (e) => { 
         e.preventDefault();
         const { email, phoneNumber, mpin } = data;
-        const response = await fetch("https://crawford2u.onrender.com/agent/logout", {
+        const response = await fetch("https://crawford-gmwe.onrender.com/agent/logout", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
